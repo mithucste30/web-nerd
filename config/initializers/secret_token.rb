57974609c3de29a4b68e83bd630124864cbf8fc3
MD. MAZHARUL ISLAM <mithu.cste.30@gmail.com>
2014-08-16 22:53:50 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebNerd::Application.config.secret_key_base = 'b8385f131c036c54a15cfc3d302771298652426b75f1d9430d1e80fec01d5e5bcdaa9b3571170819bdc0157cfd8d9f7a5607847f0ab2fc65a4dea9200a2cab14'
